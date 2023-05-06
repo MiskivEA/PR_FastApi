e3ea39ca4db957afff54e68464412b5e8305c0df
Eugene <evgenymiskiv@gmail.com>
2023-05-06 15:11:22 +0300

    create db tables

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv('infra/.env')
 
-POSTGRES_USER = os.environ.get('DB_HOST')
-POSTGRES_PASSWORD = os.environ.get('DB_PORT')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 DB_NAME = os.environ.get('DB_NAME')
 DB_PORT = os.environ.get('DB_PORT')
 DB_HOST = os.environ.get('DB_HOST')
